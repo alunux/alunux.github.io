1ff065d5d1314090ed27c3802881d4834386c698
La Ode Muh. Fadlun Akbar <fadlun.net@gmail.com>
2018-02-10 02:07:18 +0700

    remove everything

--- README.md
+++
@@ -1,1 +0,0 @@
-# alunux.github.io
--- js/partials.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $.get("partials/nav.html", function(data){
-        $("nav").html(data);
-    });
-
-    $.get("partials/footer.html", function(data){
-        $("footer").html(data);
-    });
-});
